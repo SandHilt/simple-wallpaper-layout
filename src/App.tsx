44a6626d593371c7db39e1156c1ddc374727d24f
Bruno Ochotorena <hilt@outlook.com.br>
2020-04-20 13:38:37 -0300

    Começando a migração

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,26 @@
-import 'animate.css';
-import 'holderjs';
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Card from './card/Card';
-import Footer from './footer/Footer';
-import Header from './header/Header';
-import Navigation from './navigation/Navigation';
 
-interface ISettings {
-  cards: JSX.Element[];
-  cardsPerPage: number;
-  perPageNavigation: number;
-  start: number;
-}
-
-class App extends React.Component<any, ISettings> {
-  constructor(props: any) {
-    super(props);
-    const start = 0;
-    const total = 100;
-    const cardsPerPage = 20;
-    const cards: JSX.Element[] = Array(total).fill(null);
-
-    this.state = {
-      cards,
-      cardsPerPage,
-      perPageNavigation: Math.ceil(total / cardsPerPage),
-      start,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-  public handleClick(start: number) {
-    this.setState({
-      start,
-    });
-  }
-  public componentWillMount() {
-    const { cards } = this.state;
-
-    for (let index = 0; index < cards.length; index++) {
-      const url = new URL(`/180/320?lock=${index}`, 'https://loremflickr.com');
-      cards[index] = <Card key={index} url={url} />;
-    }
-  }
-  // public componentDidUpdate() {
-  //   const images = document.getElementsByTagName('img');
-  //   for (let i = 0; i < images.length; i++) {
-  //     holder.run({
-  //       images: images.item(i),
-  //     });
-  //   }
-  // }
-  public render() {
-    const { start, cards, cardsPerPage, perPageNavigation } = this.state;
-
-    const begin = start * cardsPerPage;
-    const end = begin + cardsPerPage;
-
-    const someVisibleCards = cards.slice(begin, end);
-
-    return (
-      <main className="App">
-        <Header />
-        <Navigation
-          length={perPageNavigation}
-          start={this.state.start}
-          onClick={this.handleClick}
-        />
-        <article className="App-wall">{someVisibleCards}</article>
-        <Footer />
-      </main>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
